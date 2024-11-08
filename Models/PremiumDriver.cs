1fef617a20476ebc2df7c264cceaf7881789d98b
Dylan-bak <jesuscarecrow@gmail.com>
2024-11-08 21:52:34 +0900

    Feat: [Models] 모델 확장

---
+++ Models/PremiumDriver.cs
@@ -0,0 +1,7 @@
+namespace RideSharingInterview.Models
+{
+  public class PremiumDriver : Driver
+  {
+    public PremiumDriver(string name, int userId, string carModel, string location) : base(name, userId, carModel, location) { }
+  }
+}
---
+++ Models/StandardDriver.cs
@@ -0,0 +1,7 @@
+namespace RideSharingInterview.Models
+{
+  public class StandardDriver : Driver
+  {
+    public StandardDriver(string name, int userId, string carModel, string location) : base(name, userId, carModel, location) { }
+  }
+}
